f400a9a795768b90d2c212d798e1fb5d804fe9c8
agill248 <aveerg367@deltalearns.ca>
2025-03-06 20:30:21 +0000

    Initial Commit, made greeting.

---
+++ main.py
@@ -0,0 +1,6 @@
+# This is a Enigma Machine
+
+# Greet the user
+print("Welcome to the Enigma Machine on Python!")
+name = input("What is your name?")
+print(f"Hello {name}, I hope you have a blast!")
