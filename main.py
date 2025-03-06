991cc84e8a939a45733a8b6be4fab3a8adc0f6ff
agill248 <aveerg367@deltalearns.ca>
2025-03-06 20:59:17 +0000

    Finished the Pseudocode

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
+import random # For random fun fact
+
 # This is a Enigma Machine
 
+# Create a Encrypt Function
+
+# Create a Decrypt Function
+
+
+# Create a Atbash Fun Fact Function
+
+
+# Create a list for encrypt and decrypt
+
+
 # Greet the user
-print("Welcome to the Enigma Machine on Python!")
+print("Welcome to the Enigma Machine on Python! Today's cipher language is Atbash!")
 name = input("What is your name?")
 print(f"Hello {name}, I hope you have a blast!")
+
+
+# Ask the user if they would like to encrypt or decrypt a message
+
+
+# When user selects if they would like to encrypt or decrypt then ask them to type in their message
+
+
+# Present the user with their message encrypted or decrypted
+
+
+# Give them a menu to choose from, either encrypt/decrypt a message(restart), give a random fun fact about Atbash, or exit
